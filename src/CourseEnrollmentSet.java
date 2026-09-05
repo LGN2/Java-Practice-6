b2c7ef2dfb0ab963f5538f19b4b6d14c11f032e5
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 18:50:27 +0400

    feat (src): CourseEnrollmentSet added main method

--- src/CourseEnrollmentSet.java
+++ src/CourseEnrollmentSet.java
@@ -1,2 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class CourseEnrollmentSet {
+    public static void main(String[] args) {
+
+        Set<String> courses = new HashSet<>();
+
+        courses.add("Java");
+        courses.add("Python");
+        courses.add("Database");
+        courses.add("Web Development");
+        courses.add("Networking");
+
+        IO.println("========== COURSE ENROLLMENT ==========");
+
+        IO.println("Courses before removal:");
+        IO.println(courses);
+
+        String courseToRemove = "Networking";
+
+        if (courses.remove(courseToRemove)) {
+            IO.println("\n" + courseToRemove + " was removed.");
+        } else {
+            IO.println("\nCourse not found.");
+        }
+
+        IO.println("\nCourses after removal:");
+        IO.println(courses);
+
+        IO.println("Total Courses: " + courses.size());
+    }
 }
