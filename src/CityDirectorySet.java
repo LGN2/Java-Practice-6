10b66154e30c48f100887aa5cf7e23187064d3e2
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 18:57:07 +0400

    feat (src):CityDirectorySet added main method

--- src/CityDirectorySet.java
+++ src/CityDirectorySet.java
@@ -1,2 +1,34 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CityDirectorySet {
+
+        public static void main(String[] args) {
+
+            Set<String> cities = new TreeSet<>();
+
+            cities.add("Muscat");
+            cities.add("Nizwa");
+            cities.add("Sohar");
+            cities.add("Salalah");
+            cities.add("Sur");
+            cities.add("Ibri");
+
+            IO.println("========== CITY DIRECTORY ==========");
+
+            IO.println("Cities:");
+
+            for (String city : cities) {
+                IO.println(city);
+            }
+
+            String searchCity = "Nizwa";
+
+            if (cities.contains(searchCity)) {
+                IO.println("\n" + searchCity + " exists in the directory.");
+            } else {
+                IO.println("\n" + searchCity + " was not found.");
+            }
+            IO.println("Total Cities: " + cities.size());
+        }
 }
