6bf23281714903f35dacbf8528a51f671ef378b9
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-09-05 19:42:41 +0400

    feat (src): OnlineCourseEnrollmentSystem added main method

--- src/OnlineCourseEnrollmentSystem.java
+++ src/OnlineCourseEnrollmentSystem.java
@@ -1,2 +1,55 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class OnlineCourseEnrollmentSystem {
+
+    public static void main(String[] args) {
+
+        Set<String> students = new HashSet<>();
+
+        Map<String, String> enrollments = new HashMap<>();
+
+        enrollStudent(
+                students,
+                enrollments,
+                "Ali",
+                "Java"
+        );
+
+        enrollStudent(
+                students,
+                enrollments,
+                "Sara",
+                "Python"
+        );
+
+        enrollStudent(
+                students,
+                enrollments,
+                "Ahmed",
+                "Database"
+        );
+
+        enrollStudent(
+                students,
+                enrollments,
+                "Maha",
+                "Java"
+        );
+
+        IO.println(
+                "\n========== COURSE ENROLLMENTS =========="
+        );
+
+        displayEnrollments(enrollments);
+
+        enrollments.put("Ahmed", "Web Development");
+
+        IO.println(
+                "\nAfter updating Ahmed's course:"
+        );
+        displayEnrollments(enrollments);
+    }
 }
